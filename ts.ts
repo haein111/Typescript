eabf42af91292b59ca599c49808332fe68f89b82
key <j@gmail.com>
2024-11-01 15:30:24 +0900

    first commit

---
+++ ts.ts
@@ -0,0 +1,44 @@
+interface Car {
+  color: string;
+  wheels: number;
+  start(): void;
+}
+
+interface Benz extends Car {
+  door: number;
+  stop(): void;
+}
+
+const benz: Benz = {
+  door: 5,
+  stop() {
+    console.log("stop");
+  },
+  color: "white",
+  wheels: 3,
+  start() {},
+};
+
+class Bmw implements Car {
+  color: string;
+  wheels = 4;
+  constructor(c: string) {
+    this.color = c;
+  }
+  start() {
+    console.log("go...");
+  }
+}
+
+interface Toy {
+  color: string;
+  name: number;
+}
+
+interface Car2 {
+  name2: string;
+}
+
+interface TodyCar extends Toy, Car2 {
+  price: number;
+}
